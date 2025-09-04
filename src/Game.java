4423b0f745888462e44b226938002641ecea2502
Aaron <aarondeo30@gmail.com>
2025-09-04 11:20:29 -0700

    Created necessary helper functions

--- src/Game.java
+++ src/Game.java
@@ -13,5 +13,58 @@ public class Game {
         }
     }
 
+    public int getScore(){
+        return this.score;
+    }
+
+    private List<Dice> getDiceList(){
+        return this.diceList;
+    }
+
+    private boolean hasThrees(){
+        for (Dice d : getDiceList()){
+            if (d.getValue() == 3){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void removeThrees(){
+        getDiceList().removeIf(d -> d.getValue() == 3);
+    }
+
+    private Dice getLowestDice(){
+        Dice lowestDice = getDiceList().get(0);
+        for (Dice d : getDiceList()){
+            if (d.getValue() < lowestDice.getValue()){
+                lowestDice = d;
+            }
+        }
+        return lowestDice;
+    }
+
+    private void removeSingleDice(Dice d){
+        getDiceList().remove(d);
+    }
+
+    private void rollAllDice(){
+        for (Dice d : getDiceList()){
+            d.rollDice();
+        }
+    }
+
+    private boolean hasDice(){
+        if (getDiceList().size() == 0){
+            return false;
+        }
+        return true;
+    }
+
+    private void addValuetoScore(int value){
+        this.score = this.score + value;
+    }
+
+
 
 }
