f7a837e6d0b3e5e0b8e46abe12bbc39b6b4e2980
Aaron <aarondeo30@gmail.com>
2025-09-04 12:12:07 -0700

    changing input to match criteria

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         //GameSimulation left parameter for diceAmount, right for simAmount
-        GameSimulation sim = new GameSimulation(5, 100000);
+        GameSimulation sim = new GameSimulation(5, 10000);
         sim.simulateGames();
     }
 }
