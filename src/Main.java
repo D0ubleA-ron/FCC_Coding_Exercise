d5659604b6e96b5e869ce68c9d4fe343f98f05c1
Aaron <aarondeo30@gmail.com>
2025-09-04 12:09:22 -0700

    added clarity for running simulation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        //GameSimulation left parameter for diceAmount, right for simAmount
         GameSimulation sim = new GameSimulation(5, 100000);
         sim.simulateGames();
     }
